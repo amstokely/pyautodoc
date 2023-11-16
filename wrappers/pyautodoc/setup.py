bfb5d8a2a0525f1a96d9475253c023acbabbc2e1
amstokely <amstokely@ucsd.edu>
2023-11-16 15:58:11 -0700

    fix: Changed naming from pycautodoc to pyautodoc to make it consistent with the tutorial.

--- wrappers/pyautodoc/setup.py
+++ wrappers/pyautodoc/setup.py
@@ -8,17 +8,17 @@ path = os.path.relpath(os.path.dirname(__file__))
 setup(
     author='Andy Stokely',
     email='amstokely@ucsd.edu',
-    name='pycautodoc',
+    name='pyautodoc',
     install_requires=[],
     platforms=['Linux',
                'Unix', ],
     python_requires=">=3.8",
-    py_modules=[path + "pycautodoc/pycautodoc"],
+    py_modules=[path + "pyautodoc/pyautodoc"],
     packages=find_packages() + [''],
     zip_safe=False,
     package_data={
         '': [
-            path + '/pycautodoc/_pycautodoc.so'
+            path + '/pyautodoc/_pyautodoc.so'
         ]
     },
 )
